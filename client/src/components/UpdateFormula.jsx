9a4f5914b11f09bb79628a62662cbf6ca76bb504
Edgar Jara <ejarahernandez1@gmail.com>
2023-03-21 14:53:56 -0400

    fixed update page

--- client/src/components/UpdateFormula.jsx
+++ client/src/components/UpdateFormula.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import FileBase64 from 'react-file-base64';
 
@@ -15,6 +15,7 @@ const UpdateFormula = (props) => {
     const [loading, setLoading] = useState(true);
     const [material, setMaterial] = useState({});
     const [materials, setMaterials] = useState({});
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/oneFormula/' + id)
@@ -30,6 +31,14 @@ const UpdateFormula = (props) => {
 
     const submitHandler = (e) => {
         e.preventDefault();
+
+        axios.put(`http://localhost:8000/api/updateFormula/${id}`, formula)
+            .then(res => {
+                navigate('/');
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     const clickHandler = (e) => {
@@ -41,24 +50,31 @@ const UpdateFormula = (props) => {
         setFormula({...formula, [e.target.name]:e.target.value})
     }
 
-    const materialChangeHandler = (e) => {
-        setMaterial({...material, [e.target.name]:e.target.value})
-    }
+    // const materialChangeHandler = (e) => {
+    //     // setMaterial({...material, [e.target.name]:e.target.value})
+    // }
 
     const cchcch = (e) => {
         setMaterial({...material, [e.target.name]:e.target.value});
+        setMaterials({...materials, [e.target.id]:{...material, [e.target.name]:e.target.value}});
         setFormula({...formula, materialsNeeded:{...materials, [e.target.id]:{...material, [e.target.name]:e.target.value}}})
     }
 
-    const materialsClickHandler = (e) => {
+    // const inputOnClick = (e) => {
+    //     e.preventDefault();
+    //     setMaterial({...material, [e.target.name]:e.target.value})
+    // }
+
+    const editClickHandler = (e) => {
         e.preventDefault();
-        setMaterials({...materials, [e.target.name]:material})
+        setMaterial(materials[e.target.name])
+
     }
 
-    const saveMaterialsHandler = (e) => {
-        e.preventDefault();
-        setFormula({...formula, materialsNeeded:materials})
-    }
+    // const saveMaterialsHandler = (e) => {
+    //     e.preventDefault();
+    //     setFormula({...formula, materialsNeeded:materials})
+    // }
 
     return (
         <div>
@@ -98,21 +114,21 @@ const UpdateFormula = (props) => {
                                         <label>1. </label>
                                         <input type="text" name="name" id="material1" onChange={cchcch} value={formula.materialsNeeded.material1.name}/>
                                         <input type="number" name="amount" id="material1" onChange={cchcch} value={formula.materialsNeeded.material1.amount}/>
-                                        <button name="material1" onClick={materialsClickHandler}>Edit</button>
+                                        <button name="material1" onClick={editClickHandler}>Edit</button>
                                     </div>
                                     <div>
                                         <label>2. </label>
-                                        <input type="text" name="name" onChange={materialChangeHandler} value={formula.materialsNeeded.material2.name}/>
-                                        <input type="number" name="amount" onChange={materialChangeHandler} value={formula.materialsNeeded.material2.amount}/>
-                                        <button name="material2" onClick={materialsClickHandler}>Edit</button>
+                                        <input type="text" name="name" id="material2" onChange={cchcch} value={formula.materialsNeeded.material2.name}/>
+                                        <input type="number" name="amount" id="material2" onChange={cchcch} value={formula.materialsNeeded.material2.amount}/>
+                                        <button name="material2" onClick={editClickHandler}>Edit</button>
                                     </div>
                                     {
                                         formula.materialsNeeded.material3?
                                             <div>
                                                 <label>3. </label>
-                                                <input type="text" name="name" onChange={materialChangeHandler} value={formula.materialsNeeded.material3.name}/>
-                                                <input type="number" name="amount" onChange={materialChangeHandler} value={formula.materialsNeeded.material3.amount}/>
-                                                <button name="material3" onClick={materialsClickHandler}>Edit</button>
+                                                <input type="text" name="name" id="material3" onChange={cchcch} value={formula.materialsNeeded.material3.name}/>
+                                                <input type="number" name="amount" id="material3" onChange={cchcch} value={formula.materialsNeeded.material3.amount}/>
+                                                <button name="material3" onClick={editClickHandler}>Edit</button>
                                             </div>
                                             :
                                             null
@@ -121,9 +137,9 @@ const UpdateFormula = (props) => {
                                         formula.materialsNeeded.material4?
                                             <div>
                                                 <label>4. </label>
-                                                <input type="text" name="name" onChange={materialChangeHandler} value={formula.materialsNeeded.material4.name}/>
-                                                <input type="number" name="amount" onChange={materialChangeHandler} value={formula.materialsNeeded.material4.amount}/>
-                                                <button name="material4" onClick={materialsClickHandler}>Edit</button>
+                                                <input type="text" name="name" id="material4" onChange={cchcch} value={formula.materialsNeeded.material4.name}/>
+                                                <input type="number" name="amount" id="material4" onChange={cchcch} value={formula.materialsNeeded.material4.amount}/>
+                                                <button name="material4" onClick={editClickHandler}>Edit</button>
                                             </div>
                                             :
                                             null
@@ -132,14 +148,14 @@ const UpdateFormula = (props) => {
                                         formula.materialsNeeded.material5?
                                             <div>
                                                 <label>5. </label>
-                                                <input type="text" name="name" onChange={materialChangeHandler} value={formula.materialsNeeded.material5.name}/>
-                                                <input type="number" name="amount" onChange={materialChangeHandler} value={formula.materialsNeeded.material5.amount}/>
-                                                <button name="material5" onClick={materialsClickHandler}>Edit</button>
+                                                <input type="text" name="name" id="material5" onChange={cchcch} value={formula.materialsNeeded.material5.name}/>
+                                                <input type="number" name="amount" id="material5" onChange={cchcch} value={formula.materialsNeeded.material5.amount}/>
+                                                <button name="material5" onClick={editClickHandler}>Edit</button>
                                             </div>
                                             :
                                             null
                                     }
-                                    <button onClick={saveMaterialsHandler}>Save Materials</button>
+                                    {/* <button onClick={saveMaterialsHandler}>Save Materials</button> */}
                                 </section>
                                 <input type="submit" value="Submit Changes" />
                             </form>
@@ -151,3 +167,6 @@ const UpdateFormula = (props) => {
 }
 
 export default UpdateFormula;
+
+// for next working session: set all materials needed inputs to disable and have the edit button to enable them while setting material to the values of that input
+// update page working 
